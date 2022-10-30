0af193296f8dffaebd608cd4fa64f27c805f2946
BurAndBY <48630651+BurAndBY@users.noreply.github.com>
2022-10-30 20:05:21 +0300

    Commit from TeleportHQ

--- src/views/page.js
+++ src/views/page.js
@@ -23,20 +23,18 @@ const Page = (props) => {
       </Helmet>
       <Header rootClassName="header-root-class-name"></Header>
       <div className="page-container1">
-        <h1 className="page-text">Разделі Пакой</h1>
+        <h1 className="page-text">Жартлыст 2 Інтэрлыстыянальны</h1>
         <h1 className="page-text1">Апошняе аднаўленне 22.10.2022</h1>
-        <Link to="/str" className="page-navlink button">
-          Спампаваць
-        </Link>
+        <button className="page-button button">У праце</button>
       </div>
       <div className="page-hero">
         <div className="page-container2">
           <h1 className="page-text2">Інструкцыя па усталёўцы</h1>
           <div className="page-btn-group">
-            <Link to="/windows" className="page-navlink1 button">
+            <Link to="/windows" className="page-navlink button">
               Windows
             </Link>
-            <Link to="/linux" className="page-navlink2 button">
+            <Link to="/linux" className="page-navlink1 button">
               Linux
             </Link>
           </div>
