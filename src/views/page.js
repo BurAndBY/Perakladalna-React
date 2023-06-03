01a7dec47b7bd64d0384c414cccf3265dde4d949
BurAndBY <48630651+BurAndBY@users.noreply.github.com>
2023-06-03 21:03:33 +0300

    Commit from TeleportHQ

--- src/views/page.js
+++ src/views/page.js
@@ -23,15 +23,19 @@ const Page = (props) => {
       </Helmet>
       <Navbar rootClassName="navbar-root-class-name1"></Navbar>
       <div className="page-container1">
-        <h1 className="page-text">Celeste</h1>
-        <h1 className="page-text1">Апошняе аднаўленне 14.11.2022</h1>
+        <img
+          src="/the_jackbox_party_pack_logo-400h.png"
+          alt="image"
+          className="page-image"
+        />
+        <h1 className="page-text">Апошняе аднаўленне 03.06.2023</h1>
         <Link to="/celeste" className="page-navlink button">
           Спампаваць
         </Link>
       </div>
       <div className="page-hero">
         <div className="page-container2">
-          <h1 className="page-text2">Інструкцыя па усталёўцы</h1>
+          <h1 className="page-text1">Інструкцыя па усталёўцы</h1>
           <div className="page-btn-group">
             <Link to="/windows" className="page-navlink1 button">
               Windows
